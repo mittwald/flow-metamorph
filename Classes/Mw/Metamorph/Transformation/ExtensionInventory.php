ce016d7c9a45dbc377a4e3cffb8c90a7d356cad5
Martin Helmich <m.helmich@mittwald.de>
2014-08-18 21:46:13 +0200

    Fix missing import.

--- Classes/Mw/Metamorph/Transformation/ExtensionInventory.php
+++ Classes/Mw/Metamorph/Transformation/ExtensionInventory.php
@@ -3,6 +3,7 @@ namespace Mw\Metamorph\Transformation;
 
 
 use Mw\Metamorph\Domain\Model\MorphConfiguration;
+use Mw\Metamorph\Domain\Model\State\PackageMapping;
 use Mw\Metamorph\Domain\Service\MorphState;
 use Mw\Metamorph\Io\OutputInterface;
 
