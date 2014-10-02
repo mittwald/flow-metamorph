4f10ec342d1528631eba1d4711db38283f872e99
Martin Helmich <m.helmich@mittwald.de>
2014-10-02 20:44:52 +0200

    Prettify doc-comment modification.

--- Classes/Mw/Metamorph/Transformation/Helper/Annotation/DocCommentModifier.php
+++ Classes/Mw/Metamorph/Transformation/Helper/Annotation/DocCommentModifier.php
@@ -22,11 +22,36 @@ class DocCommentModifier
 
     public function addAnnotationToDocCommentString($commentString, AnnotationRenderer $annotation)
     {
+        // Ensure that comment opening (/**) is on single line
+        if (!preg_match(',^[\t ]*/\*\*[\t ]*\n,', $commentString))
+        {
+            $commentString = preg_replace(',/\*\*[\t ]*,', "/**\n * ", $commentString);
+        }
+
+        // Ensure that comment ending (*/) is on single line
+        if (!preg_match(',\n[\t ]*\*/[\t ]*$,', $commentString))
+        {
+            $commentString = preg_replace(',\*/[\t ]*$,', "\n */", $commentString);
+        }
+
         $lines = explode("\n", $commentString);
         $count = count($lines);
 
+        // Ensure that annotations are in a separate paragraph in longer comments.
+        if ($count > 2 && trim($lines[$count - 2]) !== '*' && !preg_match(',^[\t ]*\*[\t ]*@,', $lines[$count - 2]))
+        {
+            $lines[$count - 1] = ' *';
+            $count++;
+        }
+
         $lines[$count - 1] = ' * ' . $annotation->render();
         $lines[$count]     = ' */';
+
+        // Remove trailing whitespaces.
+        foreach ($lines as &$line)
+        {
+            $line = rtrim($line);
+        }
 
         return implode("\n", $lines);
     }
