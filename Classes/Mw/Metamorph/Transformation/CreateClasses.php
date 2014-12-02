a1f9c37f1779129663f3b08c6194002022c581d2
Martin Helmich <m.helmich@mittwald.de>
2014-12-02 14:30:59 +0100

    Improve test file path generation.

--- Classes/Mw/Metamorph/Transformation/CreateClasses.php
+++ Classes/Mw/Metamorph/Transformation/CreateClasses.php
@@ -9,6 +9,7 @@ use Mw\Metamorph\Domain\Repository\MorphConfigurationRepository;
 use Mw\Metamorph\Domain\Service\MorphExecutionState;
 use Symfony\Component\Console\Output\OutputInterface;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Package\PackageInterface;
 use TYPO3\Flow\Utility\Files;
 
 
@@ -49,9 +50,7 @@ class CreateClasses extends AbstractTransformation
             $newClassName = new String($classMapping->getNewClassName());
 
             $relativeFilename = $newClassName->replace('\\', '/')->append('.php');
-            $absoluteFilename = $this->isClassTestCase($relativeFilename)
-                ? $package->getPackagePath() . '/Tests/' . $relativeFilename->replace('Tests/', '')
-                : $package->getClassesPath() . '/' . $relativeFilename;
+            $absoluteFilename = $this->getAbsoluteFilename($relativeFilename, $package);
 
             Files::createDirectoryRecursively(dirname($absoluteFilename));
 
@@ -76,8 +75,44 @@ class CreateClasses extends AbstractTransformation
 
 
 
+    /**
+     * Determines if a class contains a test case.
+     *
+     * @param \Helmich\Scalars\Types\String $relativeFilename The relative class file name.
+     * @return bool TRUE if the class contains a test case, otherwise FALSE.
+     */
     private function isClassTestCase(String $relativeFilename)
     {
         return $relativeFilename->strip('/')->contains('Tests/') || $relativeFilename->endsWidth('Test.php');
+    }
+
+
+
+    /**
+     * Gets the absolute target filename for a class file.
+     *
+     * @param \Helmich\Scalars\Types\String $relativeFilename The relative class file name (auto-derived from class name).
+     * @param PackageInterface              $package          The target package.
+     * @return string The target filename.
+     */
+    private function getAbsoluteFilename(String $relativeFilename, PackageInterface $package)
+    {
+        if (FALSE === $this->isClassTestCase($relativeFilename))
+        {
+            return $package->getClassesPath() . '/' . $relativeFilename;
+        }
+        else
+        {
+            return (new String(''))
+                ->append($package->getPackagePath())
+                ->stripRight('/')
+                ->append('/Tests/Unit/')
+                ->append(
+                    $relativeFilename
+                        ->replace('Tests/', '')
+                        ->replace((new String($package->getPackageKey()))->replace('.', '/')->append('/'), '')
+                )
+                ->toPrimitive();
+        }
     }
 }
